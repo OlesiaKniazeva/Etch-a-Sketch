7e9927282e58f7bc5ecece01e8b295095e3bf64b
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-22 16:35:17 +0800

    add event listeners outline

--- sketch.js
+++ sketch.js
@@ -9,6 +9,11 @@ function main() {
 
   setContainerSize(container);
   createGridSquares(container, 5);
+
+  addEventsToObjects(getObjects(container, '.square'));
+  
+  let input = getUserInput();
+
 }
 
 function convertToPixels(number) {
@@ -36,10 +41,40 @@ function createGridSquares(container, amount = DEFAULT_SQUARE_AMOUNT) {
       const square = document.createElement('div');
       square.style.width = convertToPixels(square_side);
       square.style.height = convertToPixels(square_side);
-      square.classList.add('grid-square');
+      square.classList.add('square');
       rowContainer.appendChild(square);
     }
   }
+}
+
+function getObjects(container, object_name) {
+  return container.querySelectorAll(object_name);
+}
+
+function addEventsToObjects(objects) {
+  objects.forEach(addMouseEvent);
+}
+
+function addMouseEvent(object) {
+  object.addEventListener('mouseover', (event) => {
+    changeSquareColor(event.target, 'black')
+  })
+
+  object.addEventListener('mouseout', (event) => {
+    resetSquareColor(event.target);
+  })
+}
+
+function resetSquareColor(object) {
+
+}
+
+function changeSquareColor(object, color) {
+  object.style.backgroundColor = color;
+}
+
+function addPixelatedTrail() {
+
 }
 
 function getUserInput() {
