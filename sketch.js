dd6c443bb9030735e7b4f39fc5ccc21dbad9c087
Olesia Kniazeva <knyazevaolesiya@gmail.com>
2024-01-22 16:16:40 +0800

    remove global elements, refactor

--- sketch.js
+++ sketch.js
@@ -1,23 +1,21 @@
 const DEFAULT_SQUARE_AMOUNT = 16;
 const DEFAULT_SIZE = 960;
 
-const CONTAINER = document.querySelector('.container');
-const INPUT_BUTTON = document.querySelector('button.user-input');
-
 main();
 
 function main() {
-  setContainerSize();
-  createGridSquares();
+  const container = document.querySelector('.container');
+  const input_button = document.querySelector('button.user-input');
+
+  setContainerSize(container);
+  createGridSquares(container, 5);
 }
 
 function convertToPixels(number) {
   return number + 'px';
 }
 
-function setContainerSize(size = DEFAULT_SIZE) {
-  const container = document.querySelector('div.container');
-
+function setContainerSize(container, size = DEFAULT_SIZE) {
   container.style.width = convertToPixels(size);
   container.style.height = convertToPixels(size);
 }
@@ -26,13 +24,13 @@ function countSquareSize(amount, width = DEFAULT_SIZE) {
   return width / amount;
 }
 
-function createGridSquares(amount = DEFAULT_SQUARE_AMOUNT) {
+function createGridSquares(container, amount = DEFAULT_SQUARE_AMOUNT) {
   const square_side = countSquareSize(amount);
 
   for (let row = 0; row < amount; ++row) {
     console.log(row);
     let rowContainer = document.createElement('div');
-    CONTAINER.appendChild(rowContainer);
+    container.appendChild(rowContainer);
 
     for (let squareCounter = 0; squareCounter < amount; squareCounter++) {
       const square = document.createElement('div');
